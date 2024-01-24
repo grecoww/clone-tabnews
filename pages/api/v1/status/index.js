f567538e46f02f762d40a2e1f55173206b39bc1f
Nicolas Greco <132113259+grecoww@users.noreply.github.com>
2024-01-24 22:41:04 +0000

    adicionando o .env efetivamente

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,7 +2,7 @@ import database from "../../../../infra/database.js";
 
 async function status(req, res) {
   const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  console.log(result.rows[0]);
   res.status(200).json({ chave: "valor" });
 }
 
