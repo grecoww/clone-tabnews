e44927987ff69f60486a22bedf561403a63dea08
Nicolas Greco <132113259+grecoww@users.noreply.github.com>
2024-01-27 02:45:21 +0000

    database error handling and `/status` finished

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,26 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows[0]);
-  res.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+  const pgVersion = await database.query("SELECT VERSION();");
+  const parsedPgVersion = pgVersion.rows[0]["version"].substring(11, 15);
+  const pqMaxConnections = await database.query("SHOW max_connections;");
+  const pqUsedConnections = await database.query(
+    "SELECT COUNT(*) FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+  const maxPqConnections = pqMaxConnections.rows[0].max_connections;
+  const usedPqConnections = pqUsedConnections.rows[0].count;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        postgres_version: parseFloat(parsedPgVersion),
+        max_connections: parseInt(maxPqConnections),
+        in_use_connections: parseInt(usedPqConnections),
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,35 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(responseBody.updated_at).toBeDefined();
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.postgres_version).toBeDefined();
+  const parsedPgVersion = Number(
+    responseBody.dependencies.database.postgres_version,
+  );
+  expect(parsedPgVersion).toBe(
+    responseBody.dependencies.database.postgres_version,
+  );
+
+  expect(responseBody.dependencies.database.max_connections).toBeDefined();
+  const parsedMaxConnections = Number(
+    responseBody.dependencies.database.max_connections,
+  );
+  expect(parsedMaxConnections).toBe(
+    responseBody.dependencies.database.max_connections,
+  );
+
+  console.log(responseBody.dependencies.database.in_use_connections);
+  expect(responseBody.dependencies.database.in_use_connections).toBeDefined();
+  const parsedusedConnections = Number(
+    responseBody.dependencies.database.in_use_connections,
+  );
+  expect(parsedusedConnections).toBe(
+    responseBody.dependencies.database.in_use_connections,
+  );
 });
