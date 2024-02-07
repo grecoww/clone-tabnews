63b2cd22a14188c221e92ef0f8021aed81d2fa7b
Nicolas Greco <132113259+grecoww@users.noreply.github.com>
2024-02-07 00:41:36 +0000

    rmk unit tests

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,23 +1,32 @@
 import database from "infra/database";
 
-async function status(req, res) {
+async function status(request, response) {
   const updatedAt = new Date().toISOString();
-  const pgVersion = await database.query("SELECT VERSION();");
-  const parsedPgVersion = pgVersion.rows[0]["version"].substring(11, 15);
-  const pqMaxConnections = await database.query("SHOW max_connections;");
-  const pqUsedConnections = await database.query(
-    "SELECT COUNT(*) FROM pg_stat_activity WHERE datname = 'local_db';",
-  );
-  const maxPqConnections = pqMaxConnections.rows[0].max_connections;
-  const usedPqConnections = pqUsedConnections.rows[0].count;
 
-  res.status(200).json({
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
-        postgres_version: parseFloat(parsedPgVersion),
-        max_connections: parseInt(maxPqConnections),
-        in_use_connections: parseInt(usedPqConnections),
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
       },
     },
   });
