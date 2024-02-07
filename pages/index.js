8fd90b6339c3bfe3c72ddf260189a617537e21c9
Nicolas Greco <132113259+grecoww@users.noreply.github.com>
2024-02-07 00:29:03 +0000

    arrumada nos testes

---
+++ pages/index.js
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1>Se voce chegou até aqui, vá fazer algo útil sinceramente</h1>;
+}
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,32 +4,10 @@ test("GET to /api/v1/status should return 200", async () => {
 
   const responseBody = await response.json();
 
-  expect(responseBody.updated_at).toBeDefined();
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.dependencies.database.postgres_version).toBeDefined();
-  const parsedPgVersion = Number(
-    responseBody.dependencies.database.postgres_version,
-  );
-  expect(parsedPgVersion).toBe(
-    responseBody.dependencies.database.postgres_version,
-  );
-
-  expect(responseBody.dependencies.database.max_connections).toBeDefined();
-  const parsedMaxConnections = Number(
-    responseBody.dependencies.database.max_connections,
-  );
-  expect(parsedMaxConnections).toBe(
-    responseBody.dependencies.database.max_connections,
-  );
-
-  console.log(responseBody.dependencies.database.in_use_connections);
-  expect(responseBody.dependencies.database.in_use_connections).toBeDefined();
-  const parsedusedConnections = Number(
-    responseBody.dependencies.database.in_use_connections,
-  );
-  expect(parsedusedConnections).toBe(
-    responseBody.dependencies.database.in_use_connections,
-  );
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
